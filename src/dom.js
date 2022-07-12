d12a6946cb3a8826674d229a0351019f7056ceae
Matan <matanbitton2@gmail.com>
2022-07-12 19:41:16 +0300

    add func to change icon based on time of day

--- src/dom.js
+++ src/dom.js
@@ -1,8 +1,13 @@
+import parseISO from "date-fns/parseISO";
+import format from "date-fns/format";
+
 const sun = `<i class="fa-regular fa-sun"></i>`;
 const cloud = `<i class="fa-solid fa-cloud"></i>`;
 const partlyCloudy = `<i class="fa-solid fa-cloud-sun"></i>`;
 const rain = `<i class="fa-solid fa-cloud-showers-heavy"></i>`;
 const snow = '<i class="fa-solid fa-snowflake"></i>';
+const night = `<i class="fa-solid fa-moon"></i>`;
+const day = `<i class="fa-solid fa-sun"></i>`;
 
 export function createWeatherComponent(weatherObj) {
   const weather = weatherObj;
@@ -33,20 +38,30 @@ export function createWeatherForecastComponent(weatherArr) {
   const weatherForecast = document.querySelector(".weather-forecast");
   weatherForecast.innerHTML = "";
   weatherArr.forEach((item) => {
+    const containerOne = document.createElement("div");
+    containerOne.className = "container-one";
     const container = document.createElement("div");
     container.className = "container-forecast";
 
     const date = document.createElement("div");
     date.className = "date-forecast";
-    date.innerText = item.date;
+    let dateParsed = parseISO(item.date);
+    date.innerText = format(dateParsed, "EEEE p  ");
+    date.innerHTML += `<i class="fa-solid fa-clock"></i>`;
+
+    const icon = document.createElement("div");
+    icon.innerHTML = setIconBasedOnTimeOfDay(date.innerText);
 
     const temp = document.createElement("div");
+
     temp.innerText = `${Math.round(item.temp)}°c`;
+    temp.innerHTML += `<i class="fa-solid fa-temperature-high"></i>`;
     temp.className = "temp-forecast";
 
     container.append(date, temp);
+    containerOne.append(container, icon);
 
-    weatherForecast.append(container);
+    weatherForecast.append(containerOne);
   });
   return weatherForecast;
 }
@@ -83,5 +98,17 @@ function setIconBasedOnWeather(descriptionHTML) {
   if (descriptionHTML.innerText.includes("Snow")) {
     div.innerHTML = snow;
     descriptionHTML.prepend(div);
+  }
+}
+
+function setIconBasedOnTimeOfDay(time) {
+  if (
+    time.includes("9:00 PM") ||
+    time.includes("12:00 AM") ||
+    time.includes("3:00 AM")
+  ) {
+    return night;
+  } else {
+    return day;
   }
 }
