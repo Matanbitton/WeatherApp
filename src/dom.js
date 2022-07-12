76dab095b1ff10b1570cb8a122614177cd30ddfa
Matan <matanbitton2@gmail.com>
2022-07-12 16:05:05 +0300

    make func to change weather icon based on weater data

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,8 @@
+const sun = `<i class="fa-regular fa-sun"></i>`;
+const cloud = `<i class="fa-solid fa-cloud"></i>`;
+const partlyCloudy = `<i class="fa-solid fa-cloud-sun"></i>`;
+const rain = `<i class="fa-solid fa-cloud-showers-heavy"></i>`;
+
 export default function createWeatherComponent(weatherObj) {
   const weather = weatherObj;
   const htmlSkeleton = document.querySelector(".weather-component");
@@ -12,6 +17,9 @@ export default function createWeatherComponent(weatherObj) {
   cityNameHTML.innerHTML += ` <i class="fa-solid fa-map-location-dot"></i>`;
 
   descriptionHTML.innerText = titleCase(weather.description);
+
+  setIconBasedOnWeather(descriptionHTML);
+
   feelsLikeHTML.innerText = `Feels Like: ${weather.feelsLike}°c`;
   tempHTML.innerText = `${weather.temp}°c`;
   highLowTempHTML.innerText = `H: ${weather.maxTemp}°c / L: ${weather.minTemp}°c`;
@@ -26,4 +34,27 @@ function titleCase(str) {
     str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
   }
   return str.join(" ");
+}
+
+function setIconBasedOnWeather(descriptionHTML) {
+  const div = document.createElement("div");
+  if (descriptionHTML.innerText.includes("Clear")) {
+    div.innerHTML = sun;
+    descriptionHTML.prepend(div);
+  }
+  if (
+    descriptionHTML.innerText.includes("Clouds") ||
+    descriptionHTML.innerText.includes("Cloudy")
+  ) {
+    div.innerHTML = cloud;
+    descriptionHTML.prepend(div);
+  }
+  if (descriptionHTML.innerText.includes("Broken Clouds")) {
+    div.innerHTML = partlyCloudy;
+    descriptionHTML.prepend(div);
+  }
+  if (descriptionHTML.innerText.includes("Rain")) {
+    div.innerHTML = rain;
+    descriptionHTML.prepend(div);
+  }
 }
