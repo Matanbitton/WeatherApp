9002afaf771d4bfdfae47c9789b29210a2a704d7
Matan <matanbitton2@gmail.com>
2022-07-12 18:40:59 +0300

    make func to fetch 5 day forecast

--- src/api.js
+++ src/api.js
@@ -1,10 +1,9 @@
 const API_KEY = "fda162adce857de507b0fa9fb36a4afb";
 
-export default async function currWeather(cityName) {
+export async function currWeather(cityName) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
 
   const weatherData = await loadWeatherJson(url).catch(alert);
-  console.log(weatherData);
   if (weatherData) {
     const name = weatherData.name;
     const temp = Math.round(weatherData.main.temp);
@@ -30,13 +29,13 @@ export default async function currWeather(cityName) {
 
 export async function hourlyWeather(cityName) {
   const url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}&units=metric`;
-
   const weatherData = await loadWeatherJson(url).catch(alert);
   const hoursForecastList = weatherData.list;
+  const weatherArr = [];
   hoursForecastList.forEach((item) => {
-    console.log(`Time: ${item.dt_txt} Degrees: ${Math.round(item.main.temp)} `);
+    weatherArr.push({ date: item.dt_txt, temp: item.main.temp });
   });
-  console.log(weatherData);
+  return weatherArr;
 }
 
 export async function loadWeatherJson(url) {
